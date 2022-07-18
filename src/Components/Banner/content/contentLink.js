ab6f7c7f7e79e2841a87753645d4b69729b3e94a
singharatbunphim <singharatbunphim@gmail.com>
2022-07-18 22:59:12 +0700

    fix between menuLink

--- src/Components/Banner/content/contentLink.js
+++ src/Components/Banner/content/contentLink.js
@@ -3,17 +3,26 @@ import './contentLink.scss'
 
 function ContentLink() {
   return (
-    <div className='contentlink'>
-        <div className='content_items'><span>Philipines</span></div>
-        <div className='content_items'><span>Malaysia</span></div>
-        <div className='content_items'><span>Brunei</span></div>
-        <div className='content_items'><span>Combadia</span></div>
-        <div className='content_items'><span>Myanmar</span></div>
-        <div className='content_items'><span>Laos</span></div>
-        <div className='content_items'><span>Vietnam</span></div>
-        <div className='content_items'><span>Indonesia</span></div>
-        <div className='content_items'><span>Singapore</span></div>
-    </div>
+    <>
+        <div className='contentlink'>
+            <div className='content_items'><span>Philipines</span></div>
+            <div className='content_items'><span>Malaysia</span></div>
+            <div className='content_items'><span>Brunei</span></div>
+            <div className='content_items'><span>Combadia</span></div>
+            <div className='content_items'><span>Myanmar</span></div>
+            <div className='content_items'><span>Laos</span></div>
+            <div className='content_items'><span>Vietnam</span></div>
+            <div className='double_items'>
+                <div className='content_items'><span>Indonesia</span></div>
+                <div className='content_items'><span>Singapore</span></div>
+            </div>
+         </div>
+
+         
+
+
+    </>
+
   )
 }
 
